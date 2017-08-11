32ac113b16a08c2e5f147c02c454091faee36fac
François LEPORCQ <fleporcq@gmail.com>
2017-08-11 22:56:37 +0200

    feat(octree): Implémenation du getIndex

--- core/src/com/caletes/game/octree/Octree.java
+++ core/src/com/caletes/game/octree/Octree.java
@@ -1,34 +1,75 @@
 package com.caletes.game.octree;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 //cf. http://pierre-benet.developpez.com/tutoriels/algorithme-3d/octree-morton/
 public class Octree {
 
-    private List<Object> objects = new ArrayList<>();
-    private float exponent;
+    private int exponent;
+    private int size;
+
     private Octree[] children;
 
-
-    public Octree(float size) {
-        exponent = computeExponent(size);
+    public Octree(int size) {
+        if (!isPowOf2(size)) {
+            throw new IllegalArgumentException("Octree size must be a power of 2");
+        }
+        this.size = size;
+        exponent = convertSizeToExponent(size);
     }
 
-    private float computeExponent(float size) {
-        return (float) (Math.log(size) / Math.log(2));
+    private boolean isPowOf2(int x) {
+        return (x != 0) && ((x & (x - 1)) == 0);
     }
 
-    public float getExponent() {
+    public int getExponent() {
         return exponent;
     }
 
-    private void split(){
-
+    public int getSize() {
+        return size;
     }
 
-    private Octree getChild(float x, float y, float z) {
-        return null;
+    private int convertSizeToExponent(int size) {
+        return (int) (Math.log(size) / Math.log(2));
+    }
+
+
+    private Octree getChild(int x, int y, int z) throws PositionOutOfBounds {
+        Octree child = null;
+        if (children != null) {
+            child = children[getIndex(x, y, z)].getChild(x, y, z);
+        }
+        return child;
+    }
+
+    public int getIndex(int x, int y, int z) throws PositionOutOfBounds {
+        if (isOutOfBounds(x, y, z)) {
+            throw new PositionOutOfBounds(x, y, z);
+        }
+        int sub = exponent - 1;
+        return ((x >> sub) & 1) + (((y >> sub) & 1) << 1) + (((z >> sub) & 1) << 2);
+    }
+
+    private boolean isOutOfBounds(int x, int y, int z) {
+        return x < 0 || x >= size || y < 0 || y >= size || z < 0 || z >= size;
+    }
+
+    private void split() {
+        int childrenSize = size - 1;
+        children = new Octree[8];
+        children[0] = new Octree(childrenSize);
+        children[1] = new Octree(childrenSize);
+        children[2] = new Octree(childrenSize);
+        children[3] = new Octree(childrenSize);
+        children[4] = new Octree(childrenSize);
+        children[5] = new Octree(childrenSize);
+        children[6] = new Octree(childrenSize);
+        children[7] = new Octree(childrenSize);
+    }
+
+    public class PositionOutOfBounds extends Exception {
+        public PositionOutOfBounds(int x, int y, int z) {
+            super("point(" + x + "," + y + "," + z + ") is out of bounds");
+        }
     }
 }
--- core/tests/OctreeTest.java
+++ core/tests/OctreeTest.java
@@ -6,8 +6,39 @@ public class OctreeTest extends TestCase {
     @Test
     public void testComputeExponent() {
         Octree octree = new Octree(8);
-        assertEquals(3f, octree.getExponent());
+        assertEquals(3, octree.getExponent());
+        assertEquals(8, octree.getSize());
+
         Octree octree2 = new Octree(256);
-        assertEquals(8f, octree2.getExponent());
+        assertEquals(8, octree2.getExponent());
+        assertEquals(256, octree2.getSize());
+    }
+
+    @Test
+    public void testSizeNotPowOf2ThrowException() {
+        try {
+            new Octree(5);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Octree size must be a power of 2", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testPositionOutOfBoundsThrowException() {
+        Octree octree = new Octree(8);
+        try {
+            octree.getIndex(0, 0, 8);
+            fail();
+        } catch (Octree.PositionOutOfBounds e) {
+            assertEquals("point(0,0,8) is out of bounds", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testGetIndex() throws Octree.PositionOutOfBounds {
+        Octree octree = new Octree(256);
+        assertEquals(0, octree.getIndex(0, 0, 0));
+        assertEquals(7, octree.getIndex(255, 255, 255));
     }
 }
